c7256bf6f1cd628a3b92c038e9f8c726bd86cd61
kmarie12 <kelsey.aschenbeck@gmail.com>
2019-02-13 07:19:14 -0800

    TimeTravel button

--- app.js
+++ app.js
@@ -19,12 +19,30 @@ function cameraStart() {
     });
 }
 // Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
+//cameraTrigger.onclick = function() {
+//    cameraSensor.width = cameraView.videoWidth;
+//    cameraSensor.height = cameraView.videoHeight;
+//    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+//    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+//    cameraOutput.classList.add("taken");
+//};
+
+var n = 0;
+
+function changeImage() {
+  if ( n == 0 ) {
+    document.images["image"].src = "img/Building_white.png";
+    n = 1;
+  }
+  else if ( n == 1 ) {
+    document.images["image"].src = "img/AltFuture.png";
+    n = 2;
+  }
+  else {
+    document.images["image"].src = "img/Building_white.png";
+    n = 0;
+  }
+}
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
